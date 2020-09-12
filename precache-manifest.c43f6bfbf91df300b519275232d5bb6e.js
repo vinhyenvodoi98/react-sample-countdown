f64a4306b88b92ed9b7dedcc5e1c76cf60635147
hoang <vinhyenvodoi98@gmail.com>
2020-09-12 23:33:29 +0700

    Updates

---
+++ precache-manifest.c43f6bfbf91df300b519275232d5bb6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d8ff1b6819b97b438225cd01d2f8f3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3554f58406167a655caf",
+    "url": "./static/css/main.e65fee39.chunk.css"
+  },
+  {
+    "revision": "3258d5a908f778941b63",
+    "url": "./static/js/2.745afd24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.745afd24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3554f58406167a655caf",
+    "url": "./static/js/main.68025c4b.chunk.js"
+  },
+  {
+    "revision": "4d61ef1788746b156278",
+    "url": "./static/js/runtime-main.37dd0b4d.js"
+  },
+  {
+    "revision": "d4a0f2ce3c8f5468afb4e5d77ee7b018",
+    "url": "./static/media/clock-icon~bjyXqlIY.d4a0f2ce.png"
+  }
+]);
